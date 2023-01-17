39fe929d45964db36f39a33ef2239fa2140cf32b
xali1ove <xali1ove@mail.ru>
2023-01-17 23:49:52 +0300

    signatures

--- main.go
+++ main.go
@@ -4,19 +4,44 @@ import (
 	"log"
 	"net/http"
 	"html/template"
+	"os"
+	"bufio"
 )
 
+type Guestbook struct {
+	SignatureCount int
+	Signatures []string
+}
 //all errors
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
+//чтение файла
+func getSrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)  //открытие файла
+	if os.IsNotExist(err) {         //при возвращ ошибки указывающей на несуществ файл
+		return nil
+	}
+	check(err)                      //для другой ошибки
+	defer file.Close()              //закрытие файла
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())             //сообщить о любых ошибках обработки файла
+	return lines
+}
 //функция-обработчик
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getSrings("signatures.txt")
 	html, err := template.ParseFiles("view.html") //содержимое html используется для создания нового значени teamplate
 	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+	}
 	err = html.Execute(writer, nil) //содержимое шаблона записывается в ResponseWriter
 	check(err)
 }
@@ -26,3 +51,4 @@ func main() {
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
+
